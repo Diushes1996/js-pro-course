d461549b406e31a5ddf9a5c5f591e82d05fe64e5
Duishes1996 <kakakakaktus@inbox.ru>
2020-06-15 20:12:47 +0300

    add redux files

--- homework/src/index.js
+++ homework/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './homework1/App';
+import {App} from './homework5/App';
+//import {FriendStatus} from './test/App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
